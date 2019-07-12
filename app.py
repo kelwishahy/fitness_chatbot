1df5eb8d7585da7333768d583a3b46f2a30b524b
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2019-07-12 10:12:50 -0700

    Adding webhook

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+def test_intent():
+    req = request.get_json(force=True)
+    action = req.get('queryResult').get('action')
+    return {'fulfillmentText': 'This is a test response'}
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+   return make_response(jsonify(test_intent()))
 
 
 if __name__ == '__main__':
